fff00f799f38807cb4f9ec6ca37ba4a2297f4a34
saednasir <saednasir@gmail.com>
2019-07-01 00:02:27 +0530

    created db

--- parkinglot.py
+++ parkinglot.py
@@ -9,16 +9,25 @@ class Config:
          'db_file': 'parking_lot.sqlite',
          'table_name': 'parking',
          'table_fields': [
-             ['slot', 'INTEGER PRIMARY KEY'],
+             ['uid','INTEGER PRIMARY KEY'],
+             ['slot', 'INTEGER'],
              ['registration'],
              ['color'],
              ['timestamp', 'INTEGER']
           ]
       }
     }
+    
+    def __init__(self, env='dev'):
+        self._define_properties(env)
+
+    # private
+    def _define_properties(self, collection_env):
+        for (key, value) in self.CONFIGS[collection_env].items():
+            setattr(self, key, value)
 
 class Storage:
-    def __prepare_dir(self, path):
+    def _prepare_dir(self, path):
         if not os.path.exists(path):
             os.makedirs(path)
         
@@ -29,7 +38,7 @@ class Storage:
 class DBStorage(Storage):
 
     def __init__(self, config):
-        self.__prepare_dir(os.path.join(os.getcwd(),config.database_folder))
+        self._prepare_dir(os.path.join(os.getcwd(),config.database_folder))
         self.conn = sqlite3.connect(os.path.join(os.getcwd(), config.database_folder, config.db_file))
         self.conn.text_factory = str
         self.conn.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
@@ -40,31 +49,39 @@ class DBStorage(Storage):
             )
         )
         
-    @property
+    
     def create_table(self,space):
-        try:  
-            self.curr.execute(
+        try:
+            for each in range(1, space+1):
+                self.curr.execute(
                 'INSERT INTO {} ({}) VALUES ( {} );'.format(
                     config.table_name,
-                    config.table_fields[0]
-                ),  [slot for slot in range(1,space)])
+                    'slot', '?'
+                ),[each])
             self.conn.commit()
         except:
-            self.curr.execute('DROP TABLE {}'.format(config.table_name))
-            self.conn.commit()
-            self.curr.execute(
-            'create table if not exists {} ({})'.format(
-                config.table_name, ','.join([' '.join(k) for k in config.table_fields])
-                )
-            )
+            print("Inser exception raised!")
             
-        return 'Created a parking lot with' + str(space) + 'slots'
+        return 'Created a parking lot with ' + str( space ) + ' slots'
+    @property
+    def events(self):
+        """select * from events ORDER BY timestamp;"""
+        self.curr.execute(
+            'select * from {} ORDER BY timestamp'.format(
+                config.table_name),
+        )
+        events = self.curr.fetchall()
+        return events
             
     
 
 if __name__ == '__main__':
     config = Config()
+    print(config.table_fields[0][0])
     demo = DBStorage(config)
+    print(demo.create_table(4))
+    print(demo.events)
+    
    
     
     
